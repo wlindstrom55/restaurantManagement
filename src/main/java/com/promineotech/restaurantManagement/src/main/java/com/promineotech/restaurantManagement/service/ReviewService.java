244502e3446b9ac417c2be3f8bfa52a3aa1c900f
Heath C Brown <Heath.c.b@gmail.com>
2021-04-02 17:40:58 -0500

    Add files via upload

---
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/service/ReviewService.java
@@ -0,0 +1,57 @@
+package com.promineotech.restaurantManagement.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.promineotech.restaurantManagement.entity.Review;
+import com.promineotech.restaurantManagement.repository.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+private static final Logger logger = LogManager.getLogger(UserService.class);
+	
+	@Autowired
+	private ReviewRepository repo;
+	
+	public Review getReviewById(Long id) throws Exception {
+		try {
+			return repo.findOne(id);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to retrieve review: " +id, e);
+			throw e;
+		}
+	}
+	
+	public Iterable<Review> getReview() {
+		return repo.findAll();
+	}
+	
+	public Review createReview(Review review) {
+		return repo.save(review);
+	}
+	
+	public Review updateReview(Review review, Long id) throws Exception {
+		try {
+			Review oldReview = repo.findOne(id);
+			oldReview.setRest_Id(review.getRest_Id());
+			oldReview.setReviewDescription(review.getReviewDescription());
+			oldReview.setRating(review.getRating());
+			return repo.save(oldReview);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to update review: " + id, e);
+			throw new Exception("Unable to update review");
+		}
+	}
+	
+	public void deleteReview(Long id) throws Exception {
+		try {
+			repo.delete(id);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to delete review: " + id, e);
+			throw new Exception("Unable to delete review");
+		}
+	}
+}
