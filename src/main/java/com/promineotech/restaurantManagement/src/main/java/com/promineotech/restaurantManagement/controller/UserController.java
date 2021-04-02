3bc7295ecc70cb36eafe098db1208e08def7dc4f
Heath C Brown <Heath.c.b@gmail.com>
2021-04-02 17:17:27 -0500

    Adding User Controller

---
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.promineotech.restaurantManagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promineotech.restaurantManagement.entity.User;
+import com.promineotech.restaurantManagement.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Object> getCustomer(@PathVariable Long id) {
+		try {
+			return new ResponseEntity<Object>(service.getUserById(id), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Object> getUsers() {
+		return new ResponseEntity<Object>(service.getUser(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		return new ResponseEntity<Object>(service.createUser(user), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Object> updateCustomer(@RequestBody User user, @PathVariable Long id) {
+		try {
+			return new ResponseEntity<Object>(service.updateUser(user, id), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
+		try {
+			service.deleteUser(id);
+			return new ResponseEntity<Object>("Successfully deleted customer with id: " + id, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}}
