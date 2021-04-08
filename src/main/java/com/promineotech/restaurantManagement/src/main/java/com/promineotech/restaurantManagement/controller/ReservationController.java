7f1f002238b7b0303c6474e9c3cde2d473e6fc88
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-04-08 15:38:23 -0500

    Adding ReservationController

---
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/controller/ReservationController.java
@@ -0,0 +1,61 @@
+package com.promineotech.restaurantManagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.promineotech.restaurantManagement.entity.Reservation;
+import com.promineotech.restaurantManagement.service.ReservationService;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+		@Autowired
+		private ReservationService service;
+		
+		@RequestMapping(value="/{id}", method=RequestMethod.GET)
+		public ResponseEntity<Object> getReservation(@PathVariable Long id) {
+			try {
+				return new ResponseEntity<Object>(service.getReservationById(id), HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+			}
+		}
+		
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<Object> getReservations() {
+			return new ResponseEntity<Object>(service.getReservations(), HttpStatus.OK);
+		}
+		
+		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Object> createReservation(@RequestBody Reservation reservation) {
+			return new ResponseEntity<Object>(service.createReservation(reservation), HttpStatus.CREATED);
+		}
+		
+		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+		public ResponseEntity<Object> updateReservation(@RequestBody Reservation reservation, @PathVariable Long id) {
+			try {
+				return new ResponseEntity<Object>(service.updateReservation(reservation, id), HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+			}
+		}
+		
+		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+		public ResponseEntity<Object> deleteReservation(@PathVariable Long id) {
+			try {
+				service.deleteReservation(id);
+				return new ResponseEntity<Object>("Successfully deleted customer with id: " + id, HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<Object>(e.getMessage(), HttpStatus.NOT_FOUND);
+			}
+		}
+		
+		
+}
