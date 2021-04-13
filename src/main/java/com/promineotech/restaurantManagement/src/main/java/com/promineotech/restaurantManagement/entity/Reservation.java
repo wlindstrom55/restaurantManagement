d94b5e6bca0a78e40d2c6a1226be6567f06977ed
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-04-13 14:30:00 -0500

    Update Reservation.java
    added annotations

--- src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Reservation.java
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Reservation.java
@@ -1,8 +1,17 @@
 package com.promineotech.restaurantManagement.entity;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Reservation {
@@ -10,7 +19,13 @@ public class Reservation {
 		private Long id;
 		private LocalDate reservation_time; //LocalDate only represents a date. Will still need localdateattributeconverter. 
 		//try to find a way to use LocalDateTime, which has time/date
-		private int reservation_size; //maybe incorporate some kind of list of possible users instead of this simple int
+		private Set<User> reservation_group;
+		
+		@JsonIgnore
+		private Restaurant restaurant;
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
 		public Long getId() {
 			return id;
 		}
@@ -23,11 +38,23 @@ public class Reservation {
 		public void setReservation_time(LocalDate reservation_time) {
 			this.reservation_time = reservation_time;
 		}
-		public int getReservation_size() {
-			return reservation_size;
-		}
-		public void setReservation_size(int reservation_size) {
-			this.reservation_size = reservation_size;
-		}
 		
+		//TODO gave this many to many annotation. Not sure about mappedBy
+		@ManyToMany(mappedBy = "reservations")
+		public Set<User> getReservation_group() {
+			return reservation_group;
+		}
+		public void setReservation_group(Set<User> reservation_group) {
+			this.reservation_group = reservation_group;
+		}
+		//TODO added many to one. 
+		@ManyToOne
+		@JoinColumn(name = "restaurantId")
+		public Restaurant getRestaurant() {
+			return restaurant;
+		}
+		public void setRestaurant(Restaurant restaurant) {
+			this.restaurant = restaurant;
+		}
+		 
 }
