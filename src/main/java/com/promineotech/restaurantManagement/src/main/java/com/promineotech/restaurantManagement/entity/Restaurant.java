5f568ce970826842436432f168591e3fb3af3887
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-04-13 14:30:31 -0500

    Update Restaurant.java
    messed with fields, added annotations

--- src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Restaurant.java
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Restaurant.java
@@ -1,9 +1,12 @@
 package com.promineotech.restaurantManagement.entity;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Restaurant {
@@ -11,6 +14,9 @@ public class Restaurant {
 	private Long id;
 	private String restaurant_name;
 	private String restaurant_description;
+	private Set<Reservation> restaurant_res;
+	private Set<Product> restaurant_menu;
+	private Set<Review> restaurant_review;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,6 +37,31 @@ public class Restaurant {
 	}
 	public void setRestaurant_description(String restaurant_description) {
 		this.restaurant_description = restaurant_description;
+	}
+	
+	//TODO added set of reservation, added annotation to gets/sets
+	@OneToMany(mappedBy = "restaurant")
+	public Set<Reservation> getRestaurant_res() {
+		return restaurant_res;
+	}
+	public void setRestaurant_res(Set<Reservation> restaurant_res) {
+		this.restaurant_res = restaurant_res;
+	}
+	//TODO should it be same name or not?
+	@OneToMany(mappedBy = "restaurant")
+	public Set<Product> getRestaurant_menu() {
+		return restaurant_menu;
+	}
+	public void setRestaurant_menu(Set<Product> restaurant_menu) {
+		this.restaurant_menu = restaurant_menu;
+	}
+	//TODO
+	@OneToMany(mappedBy = "restaurant")
+	public Set<Review> getRestaurant_review() {
+		return restaurant_review;
+	}
+	public void setRestaurant_review(Set<Review> restaurant_review) {
+		this.restaurant_review = restaurant_review;
 	}
 	
 
