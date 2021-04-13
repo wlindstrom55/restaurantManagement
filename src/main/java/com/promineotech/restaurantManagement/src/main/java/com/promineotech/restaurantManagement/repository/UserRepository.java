85f3188c67fde6bbb6ad519777ccf59289f5d533
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-04-13 14:33:51 -0500

    Update UserRepository.java
    added findByuserName method. It is named this way so that JPA will pick up on the method name using the "userName" field concat, which I struggled with for hours.

--- src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/repository/UserRepository.java
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.promineotech.restaurantManagement.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.promineotech.restaurantManagement.entity.User;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	 public User findByuserName(String userName); //Spring data can tell based on the method name, that it will search the db 
+	 //for a user that has the username we passed in as the parameter.
 }
