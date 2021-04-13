75fb0dea34a1452e6cd749df782122770a534d0f
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2021-04-13 14:29:21 -0500

    Update Product.java
    added correct annotations, getters/setters

--- src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Product.java
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/entity/Product.java
@@ -1,5 +1,81 @@
 package com.promineotech.restaurantManagement.entity;
 
-public class Product {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Product { //renamed to product without the "s" at the end (?)
+
+	private Long id;
+	private String product_name;
+	private double price;
+	private String description;
+	
+	@JsonIgnore
+	private Restaurant restaurant;
+	
+	@JsonIgnore
+	private Order orders;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurantId")
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "orderId")
+	public Order getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Order orders) {
+		this.orders = orders;
+	}
+	
 }
