d9372026e1af4b96214d33012665ec2405a537e7
Heath C Brown <Heath.c.b@gmail.com>
2021-04-16 15:07:02 -0500

    Updates to Reservation service to accommodate the change in the enitity

--- src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/service/ReservationService.java
+++ src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/service/ReservationService.java
@@ -1,57 +1,71 @@
 package com.promineotech.restaurantManagement.service;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.promineotech.restaurantManagement.entity.Reservation;
+import com.promineotech.restaurantManagement.entity.User;
 import com.promineotech.restaurantManagement.repository.ReservationRepository;
+import com.promineotech.restaurantManagement.repository.RestaurantRepository;
+import com.promineotech.restaurantManagement.repository.UserRepository;
+import com.promineotech.restaurantManagement.util.ReservationStatus;
+
 
 @Service
 public class ReservationService {
-
 		private static final Logger logger = LogManager.getLogger(ReservationService.class);
 		
 		@Autowired
-		private ReservationRepository repo;
+		private ReservationRepository resRepo;
+		@Autowired
+		private UserRepository userRepo;
+		@Autowired
+		private RestaurantRepository restRepo;
 		
-		public Reservation getReservationById(Long id) throws Exception {
-			try {
-				return repo.findOne(id);
-			} catch (Exception e) {
-				logger.error("Exception occurred while trying to retrieve reservation: " + id, e);
-				throw e;
-			}
-		}
 		
 		public Iterable<Reservation> getReservations() {
-			return repo.findAll();
+		return resRepo.findAll();
+	}
+	
+	public Reservation createReservation(Reservation reservation) {
+		try {
+			Reservation res = initializeReservation(reservation);
+		return resRepo.save(res);
+		} catch (Exception e) {
+			logger.error("Exception occurred while creating reservation: " , e);
+			throw e;
 		}
+
+	}	
+	
+	private Reservation initializeReservation(Reservation reservation) {
+			Reservation reserv = new Reservation();
+			reserv.setReservation_size(reservation.getReservation_size());
+			reserv.setReservation_time(reservation.getReservation_time());
+			reserv.setStatus(ReservationStatus.SUBMITTED);
+			return reserv;
 		
-		public Reservation createReservation(Reservation reservation) {
-			return repo.save(reservation);
+	}
+	public Reservation updateReservation(Reservation reservation, Long id) throws Exception {
+		try {
+			Reservation oldReservation = resRepo.findOne(id);
+			oldReservation.setReservation_time(reservation.getReservation_time());
+			oldReservation.setReservation_size(reservation.getReservation_size());
+			oldReservation.setStatus(ReservationStatus.SUBMITTED);
+			return resRepo.save(oldReservation);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to update reservation: " + id, e);
+			throw new Exception("Unable to update reservation.");
 		}
-		
-		public Reservation updateReservation(Reservation reservation, Long id) throws Exception {
-			try {
-				Reservation oldReservation = repo.findOne(id);
-				oldReservation.setReservation_time(reservation.getReservation_time());
-				oldReservation.setReservation_group(reservation.getReservation_group());
-				return repo.save(oldReservation);
-			} catch (Exception e) {
-				logger.error("Exception occurred while trying to update reservation: " + id, e);
-				throw new Exception("Unable to update reservation.");
-			}
+	}
+	
+	public void deleteReservation(Long id) throws Exception {
+		try {
+			resRepo.delete(id);
+		} catch (Exception e) {
+			logger.error("Exception occurred while trying to delete reservation: " + id, e);
+			throw new Exception("Unable to delete reservation.");
 		}
-		
-		public void deleteReservation(Long id) throws Exception {
-			try {
-				repo.delete(id);
-			} catch (Exception e) {
-				logger.error("Exception occurred while trying to delete reservation: " + id, e);
-				throw new Exception("Unable to delete reservation.");
-			}
-		}
-		
+	}
+	
 }
